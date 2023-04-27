18bd50366cbe50946c19ebed62ba7f0e02574557
hm243695czl <1224311695@qq.com>
2023-04-27 16:15:59 +0800

    提交遗漏文件

---
+++ src/api/exam/paper.ts
@@ -0,0 +1,7 @@
+export const basePaperApi = 'admin/exam/question/';
+
+export const getPaperPageApi = `${basePaperApi}page`;
+export const createPaperApi = `${basePaperApi}create`;
+export const updatePaperApi = `${basePaperApi}update`;
+export const viewPaperApi = `${basePaperApi}view`;
+export const deletePaperApi = `${basePaperApi}delete`;
