635af8f1eb2cf1744bdb513b89a78057de3c1b95
HM243695CZL <1224311695@qq.com>
2023-06-29 21:37:54 +0800

    test

--- src/layout/footer/index.vue
+++ src/layout/footer/index.vue
@@ -4,7 +4,7 @@
 			<div class="mt5">
 				<div style="width:300px;margin:0 auto;">
 					<a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=52010302003365" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
-						<img src="src/assets/img/beian.png" style="float:left;"/>
+						<img :src="beianImg" style="float:left;"/>
 						<p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">贵公网安备 52010302003365号</p>
 					</a>
 				</div>
@@ -14,6 +14,7 @@
 </template>
 
 <script setup lang="ts" name="layoutFooter">
+import beianImg from '/@/assets/img/beian.png'
 // 此处需有内容（注释也得），否则缓存将失败
 </script>
 
