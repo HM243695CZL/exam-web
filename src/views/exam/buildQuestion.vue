bfaa35e9845c76012b973ddd5615f7b97dce50df
HM243695CZL <1224311695@qq.com>
2023-07-06 16:24:22 +0800

    添加科技常识的题目

--- src/views/exam/buildQuestion.vue
+++ src/views/exam/buildQuestion.vue
@@ -54,11 +54,11 @@ export default defineComponent({
 			const qArr = [];
 			for (let i = 0; i < indexArr.length; i++) {
 				if (i === 0) {
-					qArr.push(str.slice(3, indexArr[1] - 1));
+					qArr.push(str.slice(3, indexArr[1] - 2));
 				} else if(i >= 8) {
-					qArr.push(str.slice(indexArr[i] + 3, indexArr[i + 1] - 1))
+					qArr.push(str.slice(indexArr[i] + 3, indexArr[i + 1] - 2))
 				} else {
-					qArr.push(str.slice(indexArr[i] + 3, indexArr[i + 1] - 1))
+					qArr.push(str.slice(indexArr[i] + 3, indexArr[i + 1] - 2))
 				}
 			}
 			qArr.splice(-1);
@@ -96,7 +96,7 @@ export default defineComponent({
 			const qArr = [];
 			for (let i = 0; i < indexArr.length; i++) {
 				if (i === 0) {
-					qArr.push(str.slice(8, indexArr[1] - 2));
+					qArr.push(str.slice(6, indexArr[1] - 2));
 				} else if(i >= 8) {
 					qArr.push(str.slice(indexArr[i] + 6, indexArr[i + 1] - 2))
 				} else {
